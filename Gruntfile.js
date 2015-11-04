cb720baa9dd4d512f7975dae5266372882877c29
Steven Edouard <sedouard@microsoft.com>
2015-11-04 11:16:28 -0800

    feat(infrastructure) - Initial module
    Added test framework, static validation
    module layout and package json necessary.

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+var grunt = require('grunt');
+require('load-grunt-tasks')(grunt);
+var files = ['lib/**/*.js', 'app.js', 'test/**/*.js'];
+
+grunt.initConfig({
+    mochacli: {
+        options: {
+            reporter: 'spec',
+            bail: true
+        },
+        all: ['test/*.js']
+    },
+    jshint: {
+        files: files,
+        options: {
+            jshintrc: './.jshintrc'
+        }
+    },
+    jscs: {
+        files: {
+            src: files
+        },
+        options: {
+            config: '.jscsrc',
+            esnext: true
+        }
+    },
+    jsbeautifier: {
+        test: {
+            files: {
+                src: files
+            },
+            options: {
+                mode: 'VERIFY_ONLY',
+                config: '.beautifyrc'
+            }
+        },
+        write: {
+            files: {
+                src: files
+            },
+            options: {
+                config: '.beautifyrc'
+            }
+        }
+    },
+    watch: {
+        scripts: {
+            files: files,
+            tasks: ['test'],
+            options: {
+                spawn: false,
+            },
+        },
+    }
+});
+grunt.registerTask('test', ['jshint', 'jscs', 'mochacli:all']);
